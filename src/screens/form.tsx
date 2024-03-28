1bcec1077f153923ba0f2dcc780f1e247ddbc646
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-27 21:14:42 -0500

    add form Screen

---
+++ src/screens/form.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function FormScreen(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Form Screen</Text>
+    </View>
+  );
+}
+
+export default FormScreen;
