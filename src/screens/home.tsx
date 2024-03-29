99c50372e46837c9caa7379ebe7cab5418d6ce62
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-29 18:40:04 -0500

    add ListView and update View for Box

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,33 +1,37 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {useEffect} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
-import { RootStackParamList } from '../navigation/types';
-
+import {Button, StyleSheet, Text} from 'react-native';
+import {RootStackParamList} from '../navigation/types';
+import ListView from '../components/listView';
+import Box from '../components/box';
 
 type Props = NativeStackScreenProps<RootStackParamList>;
 function HomeScreen({navigation}: Props): React.JSX.Element {
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <Button title="+" onPress={() => navigation.navigate('CreateExpenseScreen')} />
+        <Button
+          title="+"
+          onPress={() => navigation.navigate('CreateExpenseScreen')}
+        />
       ),
     });
   }, [navigation]);
 
   return (
-    <View style={styles.container}>
+    <Box
+      flex={1}
+      backgroundColor={'mainBackground'}
+      alignItems={'center'}
+      borderColor={'cardPrimaryBackground'}
+      borderWidth={1}>
       <Text style={styles.text}>Home</Text>
-    </View>
+      <ListView />
+    </Box>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   text: {
     color: 'black',
     fontSize: 42,
