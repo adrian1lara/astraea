52a30092a207cc4c7ef112a944c320c0bc16c022
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-29 17:24:42 -0500

    clean up code and optimize

--- App.tsx
+++ App.tsx
@@ -1,31 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import HomeScreen from './src/screens/home';
-import FormScreen from './src/screens/form';
-import StackParamList from './src/types/navigationTypes';
-import {Button} from 'react-native';
 import {ThemeProvider} from '@shopify/restyle';
 import theme from './src/themes/default';
-
-const Stack = createNativeStackNavigator<StackParamList>();
+import Navigation from './src/navigation';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={() => ({
-              headerTransparent: true,
-              title: '',
-              headerRight: () => <Button title="+" />,
-            })}
-          />
-          <Stack.Screen name="Form" component={FormScreen} />
-        </Stack.Navigator>
+        <Navigation />
       </NavigationContainer>
     </ThemeProvider>
   );
