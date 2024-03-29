2af188688153c81681eefc4e3010061d52e6a37f
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-29 17:25:00 -0500

    add new input and log the output

--- src/screens/form.tsx
+++ src/screens/form.tsx
@@ -1,16 +1,45 @@
 import React, {useState} from 'react';
-import {TextInput} from 'react-native';
+import {Button, StyleSheet, TextInput} from 'react-native';
 import Box from '../components/box';
 import Text from '../components/text';
 
 function FormScreen(): React.JSX.Element {
   const [name, setName] = useState('');
+  const [cost, setCost] = useState('');
+
+  const handleAdd = () => {
+    console.log(name, cost);
+  };
+
   return (
-    <Box>
+    <Box padding={'m'}>
       <Text variant={'header'}>Home</Text>
-      <TextInput value={name} onChangeText={setName} placeholder="name" />
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="name"
+      />
+      <TextInput
+        style={styles.input}
+        value={cost}
+        onChangeText={setCost}
+        placeholder="cost"
+      />
+
+      <Button title="add" onPress={handleAdd} />
     </Box>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 2,
+    borderWidth: 1,
+    margin: 5,
+    borderRadius: 5,
+    fontSize: 20,
+  },
+});
 
 export default FormScreen;
