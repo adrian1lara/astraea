34d748c075ae4b06cc8c9615422463e44ef1c221
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-30 13:03:14 -0500

    add callback to load data in the app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {ThemeProvider} from '@shopify/restyle';
 import theme from './src/themes/default';
 import Navigation from './src/navigation';
+import connectToDatabase, {createTables, getTablesNames} from './src/db/db';
 
 function App(): React.JSX.Element {
+  const loadData = useCallback(async () => {
+    try {
+      const db = await connectToDatabase();
+      await createTables(db);
+      const name = await getTablesNames(db);
+      console.log(name);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
