d5dd8c0993731b87d639046735dfd2494da2f334
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-31 17:39:39 -0500

    add getTopCostItems and update getItems query

--- src/db/items.ts
+++ src/db/items.ts
@@ -1,14 +1,15 @@
 import {SQLiteDatabase} from 'react-native-sqlite-storage';
 
 type Item = {name: string; date_added: Date; category: string; cost: number};
-
+type TopItemProps = {name: string};
 // function to add new Item
 export const addItem = async (db: SQLiteDatabase, item: Item) => {
   const insertQuery = `
     INSERT INTO ITEMS (name, date_added, category, cost)
     VALUES (?, ?, ?, ?)`;
 
-  const values = [item.name, item.date_added, item.category, item.cost];
+  const date = item.date_added.toISOString();
+  const values = [item.name, date, item.category, item.cost];
 
   try {
     return db.executeSql(insertQuery, values);
@@ -18,10 +19,35 @@ export const addItem = async (db: SQLiteDatabase, item: Item) => {
   }
 };
 
+export const getTopCostItems = async (
+  db: SQLiteDatabase,
+): Promise<TopItemProps[]> => {
+  const query = `
+  SELECT name, cost FROM Items
+  ORDER BY cost DESC
+  LIMIT 3`;
+  try {
+    const items: Item[] = [];
+    const results = await db.executeSql(query);
+    results?.forEach(result => {
+      for (let index = 0; index < result.rows.length; index++) {
+        items.push(result.rows.item(index));
+      }
+    });
+    return items;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to get Items');
+  }
+};
+
+// function to get all items
 export const getItems = async (db: SQLiteDatabase): Promise<Item[]> => {
   try {
     const items: Item[] = [];
-    const results = await db.executeSql('SELECT * FROM Items');
+    const results = await db.executeSql(
+      'SELECT * FROM Items ORDER BY date_added DESC',
+    );
     results?.forEach(result => {
       for (let index = 0; index < result.rows.length; index++) {
         items.push(result.rows.item(index));
