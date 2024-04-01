1c29c7f7166f97f9bae090870eb0b490d28ea07c
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-31 23:09:54 -0500

    add props to display colors in chart by the cost

--- src/components/donutChart.tsx
+++ src/components/donutChart.tsx
@@ -2,23 +2,45 @@ import React from 'react';
 import Box from './box';
 import Text from './text';
 import PieChart from 'react-native-pie-chart';
+import {generateSliceColors} from '../utils/randomColor';
 
-function DonutChart(): React.JSX.Element {
+type ItemProps = {
+  name: string;
+  date_added: Date;
+  category: string;
+  cost: number;
+};
+
+type Items = {
+  items: ItemProps[];
+};
+
+function DonutChart({items}: Items): React.JSX.Element {
   const widthAndHeight = 150;
-  const series = [100, 120, 3, 400, 9];
-  const sliceColor = ['#fbd203', '#ffb300', '#ff9100', '#ff6c00', '#ff3c00'];
 
+  const validItems = items.filter(item => item.cost !== 0); // Filter out items with zero cost
+  const costData = validItems.map(item => item.cost);
+  const sliceColor = generateSliceColors(costData.length);
+  //const colorMapping = costData.map((_, index) => sliceColor[index]); // Map cost to color
   return (
-    <Box padding={'s'}>
+    <Box p={'s'}>
       <Text variant={'subheader'} textAlign={'center'}>
         Basic Chart
       </Text>
-      <PieChart
-        widthAndHeight={widthAndHeight}
-        series={series}
-        sliceColor={sliceColor}
-        coverRadius={0.45}
-      />
+      {validItems.length > 0 ? ( // Check for valid items before rendering
+        <>
+          <PieChart
+            widthAndHeight={widthAndHeight}
+            series={costData}
+            sliceColor={sliceColor}
+            coverRadius={0.45}
+          />
+        </>
+      ) : (
+        <Text variant={'paragraph'} textAlign={'center'}>
+          No data available
+        </Text>
+      )}
     </Box>
   );
 }
