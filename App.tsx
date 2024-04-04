22133b82b6c69c5d3f3a530ec21c964289b43394
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-04-04 10:16:58 -0500

    add app context hook memo  and status bar style

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React, {useCallback, useEffect} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import {ThemeProvider} from '@shopify/restyle';
-import theme from './src/themes/default';
+import theme, {darkTheme} from './src/themes/default';
 import Navigation from './src/navigation';
 import connectToDatabase, {createTables, getTablesNames} from './src/db/db';
 import {StatusBar} from 'react-native';
+import {AppContext} from './src/context/appContext';
 
 function App(): React.JSX.Element {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const appContext = useMemo(() => {
+    return {
+      isDarkMode,
+      setIsDarkMode,
+    };
+  }, [isDarkMode]);
+
   const loadData = useCallback(async () => {
     try {
       const db = await connectToDatabase();
@@ -23,10 +33,12 @@ function App(): React.JSX.Element {
   }, [loadData]);
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar barStyle={'dark-content'} />
+    <ThemeProvider theme={isDarkMode ? darkTheme : theme}>
+      <StatusBar barStyle={darkTheme ? 'dark-content' : 'light-content'} />
       <NavigationContainer>
-        <Navigation />
+        <AppContext.Provider value={appContext}>
+          <Navigation />
+        </AppContext.Provider>
       </NavigationContainer>
     </ThemeProvider>
   );
