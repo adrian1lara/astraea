bdbe38bdbbd93ae27dfcaf30f51a07dd45ed2b90
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-04-05 17:00:38 -0500

    add comment

--- src/context/appContext.ts
+++ src/context/appContext.ts
@@ -4,7 +4,7 @@ type AppContextProps = {
   isDarkMode: boolean;
   setIsDarkMode: (value: boolean) => void;
 };
-
+// Theme context
 export const AppContext = createContext<AppContextProps>({
   isDarkMode: false,
   setIsDarkMode: () => {},
