f407b0007995cc39ab9dbb2b209c1af97c8c5d94
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-04-05 17:01:28 -0500

    add background and title color to handle darkmode

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,19 +1,30 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from './types';
 import HomeScreen from '../screens/home';
-import React from 'react';
+import React, {useContext} from 'react';
 import FormScreen from '../screens/form';
+import {AppContext} from '../context/appContext';
+import theme, {darkTheme} from '../themes/default';
+import Text from '../components/text';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
-
 function Navigation(): React.JSX.Element {
+  const {isDarkMode} = useContext(AppContext);
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: isDarkMode
+            ? darkTheme.colors.mainBackground
+            : theme.colors.mainBackground,
+        },
+      }}>
       <Stack.Screen
         name="HomeScreen"
         component={HomeScreen}
         options={{
-          title: 'Astraea',
+          headerTitle: () => <Text variant={'body'}>Astraea</Text>,
+          headerShadowVisible: false,
         }}
       />
       <Stack.Screen
