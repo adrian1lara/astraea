ce53e410e2e4b4f19286e96fc0f3acad0928df0b
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-04-07 22:04:49 -0500

    add useEffect too get the saved theme

--- src/components/customDrawer.tsx
+++ src/components/customDrawer.tsx
@@ -1,11 +1,22 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {AppContext} from '../context/appContext';
 import Switcher from './switcher';
 import Box from './box';
 import Text from './text';
+import {storage} from '../utils/mmkvStorage';
 
 const CustomDrawerContent = () => {
   const {isDarkMode, setIsDarkMode} = useContext(AppContext);
+
+  useEffect(() => {
+    const getInitial = async () => {
+      const storeMode = storage.getBoolean('isDarkMode');
+      setIsDarkMode(storeMode ?? false);
+    };
+
+    getInitial();
+  }, [setIsDarkMode]);
+
   return (
     <Box m={'s'}>
       <Text variant={'body'}>{isDarkMode ? 'Dark' : 'White'}</Text>
