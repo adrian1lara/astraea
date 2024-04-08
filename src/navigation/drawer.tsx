93526189075da9865fe262ac8e507fa55278c292
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-04-07 22:06:22 -0500

    handle darkmode in drawer style

--- src/navigation/drawer.tsx
+++ src/navigation/drawer.tsx
@@ -3,17 +3,29 @@ import {
   DrawerItemList,
   createDrawerNavigator,
 } from '@react-navigation/drawer';
-import React from 'react';
+import React, {useContext} from 'react';
 import Navigation from './index';
 import CustomDrawerContent from '../components/customDrawer';
+import {AppContext} from '../context/appContext';
+import theme, {darkTheme} from '../themes/default';
 
 const Drawer = createDrawerNavigator();
 
 function Root(): React.JSX.Element {
+  const {isDarkMode} = useContext(AppContext);
+
   return (
     <Drawer.Navigator
       screenOptions={{
         headerShown: false,
+        drawerStyle: {
+          backgroundColor: isDarkMode
+            ? darkTheme.colors.mainBackground
+            : theme.colors.mainBackground,
+        },
+        drawerActiveTintColor: isDarkMode
+          ? theme.colors.mainBackground
+          : darkTheme.colors.mainBackground,
       }}
       drawerContent={props => {
         return (
