5ab73a726c61bb1f8126b4496505baed24335be8
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-04-07 22:05:54 -0500

    add function to toggle darkmode and save in mmvk storage

--- src/components/switcher.tsx
+++ src/components/switcher.tsx
@@ -1,15 +1,23 @@
 import {Switch} from 'react-native';
 import Box from './box';
+import {storage} from '../utils/mmkvStorage';
 
 type SwitcherProps = {
   isDarkMode: boolean;
   setIsDarkMode: (value: boolean) => void;
 };
 
-const Switcher: React.FC<SwitcherProps> = ({isDarkMode, setIsDarkMode}) => (
-  <Box>
-    <Switch value={isDarkMode} onChange={() => setIsDarkMode(!isDarkMode)} />
-  </Box>
-);
+const Switcher: React.FC<SwitcherProps> = ({isDarkMode, setIsDarkMode}) => {
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    storage.set('isDarkMode', !isDarkMode);
+  };
+
+  return (
+    <Box>
+      <Switch value={isDarkMode} onChange={toggleDarkMode} />
+    </Box>
+  );
+};
 
 export default Switcher;
