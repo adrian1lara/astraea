ed77248c469ecf3d915072baeaea1739fcb0a2bf
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-04-17 22:18:34 -0500

    add remove category func

--- src/components/CategoryModalChildren.tsx
+++ src/components/CategoryModalChildren.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import Box from './box';
 import Text from './text';
+import {Pressable} from 'react-native';
 
 type ModalChildrenProps = {
-  name: string | null;
+  name: string;
+  removeCategory: (value: string) => void;
 };
 
-export const CategoryModalChildren: React.FC<ModalChildrenProps> = ({name}) => (
-  <Box backgroundColor={'mainBackground'}>
-    <Text variant={'body'}>{name ? name : 'some randome'}</Text>
+export const CategoryModalChildren: React.FC<ModalChildrenProps> = ({
+  name,
+  removeCategory,
+}) => (
+  <Box backgroundColor={'mainBackground'} height={'100%'}>
+    <Text variant={'subheader'} p={'m'} marginHorizontal={'m'}>
+      {name ? name : 'some randome'}
+    </Text>
+
+    <Box
+      borderWidth={1}
+      p={'m'}
+      mb={'s'}
+      marginHorizontal={'m'}
+      borderRadius={6}
+      borderColor={'secondaryText'}>
+      <Pressable onPress={() => console.log('edit')}>
+        <Text variant={'body'}>Edit</Text>
+      </Pressable>
+    </Box>
+    <Box
+      borderWidth={1}
+      p={'m'}
+      marginHorizontal={'m'}
+      borderRadius={6}
+      borderColor={'secondaryText'}>
+      <Pressable onPress={() => removeCategory(name)}>
+        <Text variant={'body'}>Delete</Text>
+      </Pressable>
+    </Box>
   </Box>
 );
