d091136622a8d07d552257214a5aef3e98d4b7d4
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-04-17 19:29:27 -0500

    update categoryitem and handle openModal

--- src/components/categoryItem.tsx
+++ src/components/categoryItem.tsx
@@ -1,23 +1,37 @@
-import {DrawerItem} from '@react-navigation/drawer';
-import React from 'react';
-import {Title} from './headerTitle';
+import React, {RefObject} from 'react';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Box from './box';
+import Text from './text';
+import {Modalize} from 'react-native-modalize';
 
 export type CategoryItemProps = {
   label: string;
   onPress: () => void;
   focused: boolean;
+  modalizeRef: RefObject<Modalize>;
 };
 
 export const CategoryItem: React.FC<CategoryItemProps> = ({
   label,
   onPress,
   focused,
-}) => (
-  <DrawerItem
-    label={() => Title({textTitle: label})}
-    onPress={onPress}
-    focused={focused}
-    inactiveBackgroundColor="rgba(14, 205, 157, 0.07)"
-    activeBackgroundColor="rgba(14, 205, 157, 0.20)"
-  />
-);
+  modalizeRef,
+}) => {
+  const onOpenModal = () => {
+    modalizeRef.current?.open();
+    console.log(modalizeRef.current);
+  };
+
+  return (
+    <Box
+      backgroundColor={focused ? 'activeDrawerItem' : 'inactiveDrawerItem'}
+      p={'sm'}
+      m={'s'}
+      marginBottom={'xs'}
+      borderRadius={5}>
+      <TouchableOpacity onPress={onPress} onLongPress={onOpenModal}>
+        <Text variant={'body'}>{label}</Text>
+      </TouchableOpacity>
+    </Box>
+  );
+};
