be63378222669bd397a4594d17fa285fe17948e0
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-04-17 22:18:15 -0500

    handle darkmode in modal

--- src/components/categoryModal.tsx
+++ src/components/categoryModal.tsx
@@ -1,20 +1,28 @@
 import {Modalize} from 'react-native-modalize';
 import React, {ReactNode, RefObject} from 'react';
+import theme, {darkTheme} from '../themes/default';
 
 type CategoryModalProps = {
   modalizeRef: RefObject<Modalize>;
   children: ReactNode;
+  isDarkMode: boolean;
 };
 
 export const CategoryModal: React.FC<CategoryModalProps> = ({
   modalizeRef,
   children,
+  isDarkMode,
 }) => (
   <Modalize
     ref={modalizeRef}
     snapPoint={300}
     withReactModal={true}
-    modalHeight={600}>
+    modalHeight={600}
+    modalStyle={{
+      backgroundColor: isDarkMode
+        ? darkTheme.colors.mainBackground
+        : theme.colors.mainBackground,
+    }}>
     {children}
   </Modalize>
 );
